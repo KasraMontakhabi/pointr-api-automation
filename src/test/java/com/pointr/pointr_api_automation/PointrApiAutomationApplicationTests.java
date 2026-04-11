c8e1f6002fdaf5368415b01bdf366252be77a0cf
kasra <montakhabikasra@gmail.com>
2026-04-11 14:32:00 +0300

    chore: initial project scaffold

---
+++ src/test/java/com/pointr/pointr_api_automation/PointrApiAutomationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pointr.pointr_api_automation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PointrApiAutomationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
